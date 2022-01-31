e5f24c4c2d45ba98071457a0aa59b66b1391b486
JetUp iMac 3 <jetupimac3@iMac-JetUp-3.local>
2022-01-31 12:38:20 +0200

    Initial commit

--- ios/Pods/Target Support Files/Pods-budget-budgetTests/Pods-budget-budgetTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_budget_budgetTests : NSObject
-@end
-@implementation PodsDummy_Pods_budget_budgetTests
-@end
---
+++ ios/Pods/Target Support Files/Pods-wordslearning-wordslearningTests/Pods-wordslearning-wordslearningTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_wordslearning_wordslearningTests : NSObject
+@end
+@implementation PodsDummy_Pods_wordslearning_wordslearningTests
+@end
